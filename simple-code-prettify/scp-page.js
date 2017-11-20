e01d1877e44c0996e88420d3f6d5f1a5a9560c58
Yuki AOI <ja01youki@gmail.com>
2017-11-20 21:51:30 +0900

    titleを設定できるように変更
    cssのタブを設定

--- simple-code-prettify/scp-page.js
+++ simple-code-prettify/scp-page.js
@@ -4,5 +4,14 @@
       contenteditable: true,
       spellcheck: false,
     });
+    /*
+    $('pre.scp[title]').each(function(){
+      var title = $(this).attr('title');
+      $(this).attr('title', '');
+      var div = $('<div>').html(title)
+        .addClass('scp-title');
+      $(this).before(div);
+    });
+    */
   });
 })(jQuery);
--- simple-code-prettify/scp-rich-editor.js
+++ simple-code-prettify/scp-rich-editor.js
@@ -1,8 +1,4 @@
 (function($){
-  function cleanhtml(s) {
-    return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-  }
-
   tinymce.PluginManager.add('scpEditor', function( editor, url ) {
     editor.addButton('scpInputCode', {
         title: 'Insert Code',
@@ -10,16 +6,26 @@
         onclick: function() {
           var width = $(window).width(), H = $(window).height(), W = ( 720 < width ) ? 720 : width;
           W = W - 40; H = H - 84;
-          tb_show( 'Insert Code', '#TB_inline?width=' + W + '&height=' + H + '&inlineId=scp-e-form' );
-          $('#scp-e-code').focus();
+          tb_show('Insert Code', '#TB_inline?width=' + W + '&height=' + H + '&inlineId=scp-e-form');
+          $('#scp-e-code').val("");
+          $('#scp-e-title').val("");
+          $('#scp-e-title').focus();
         }
       });
   });
 
   $(function(){
     var form = $('<div>').attr('id', 'scp-e-form');
+
+    $('<input>').attr('id', 'scp-e-title').attr('name', 'title').attr('type', 'text')
+      .css('width', '100%').css('display', 'block')
+      .attr('placeholder', 'Code Title')
+      .attr('accesskey', 't')
+      .attr('tabindex', '101')
+      .appendTo(form);
+
     $('<select>').attr('id', 'scp-e-lang').attr('name', 'lang')
-      .css('float', 'left').attr('accesskey', 'l')
+      .css('float', 'left')
       .append($('<option>').attr('value', '').text('- Language select -'))
       .append($('<option>').attr('value', 'lang-sh' ).text('BASH'))
       .append($('<option>').attr('value', 'lang-c'  ).text('C'))
@@ -38,12 +44,16 @@
       .append($('<option>').attr('value', 'lang-xml').text('XML'))
       .append($('<option>').attr('value', 'lang-vb').text('Visual BASIC'))
       //.append($('<option>').attr('value', 'lang-').text(''))
+      .attr('accesskey', 'l')
+      .attr('tabindex', '102')
       .appendTo(form);
 
     $('<input>').attr('id', 'scp-e-submit').attr('name', 'submit').attr('type', 'button')
       .css('float', 'right')
-      .attr('addClass', 'button-primary').attr('accesskey', 'i')
+      .addClass('button-primary')
       .attr('value', 'Insert this code')
+      .attr('accesskey', 'i')
+      .attr('tabindex', '104')
       .appendTo(form);
 
     $('<br>').css('clear', 'both')
@@ -51,15 +61,36 @@
 
     $('<textarea>').attr('id', 'scp-e-code').attr('name', 'code')
       .css('width', '100%').css('height', '85%').css('margin', '10px 0')
-      .attr('placeholder', 'Paste or enter the code here.').attr('accesskey', 'p')
+      .attr('placeholder', 'Paste or enter the code here.')
+      .attr('accesskey', 'p')
+      .attr('tabindex', '103')
       .appendTo(form);
       
     form.appendTo('body').hide();
     form.find('#scp-e-submit').click(function(){
-      var shortcode = '<pre class="scp"><code class="prettyprint linenums ' + $('#scp-e-lang').val() + '">' + cleanhtml($('#scp-e-code').val()) + '</code></pre>';
-      tinyMCE.activeEditor.execCommand('mceInsertRawHTML', 0, shortcode);
-      $('#scp-e-code').val("");
+      var div = $('<div>');
+
+      var pre = $('<pre>').addClass('scp');
+      pre.appendTo(div);
+
+      if ($('#scp-e-title').val().length > 0) {
+        //pre.attr('title', escape($('#scp-e-title').val()));//
+        $('<div>').addClass('scp-title')
+          .text($('#scp-e-title').val())
+          .prependTo(div);
+      }
+      $('<code>').addClass('prettyprint').addClass('linenums').addClass($('#scp-e-lang').val())
+        //.html(escape($('#scp-e-code').val()))
+        .text($('#scp-e-code').val())
+        .appendTo(pre);
+      tinyMCE.activeEditor.execCommand('mceInsertRawHTML', 0, div.html());
       tb_remove();
     });
   });
+
+  /*
+  function escape(s) {
+    return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+  }
+  */
 })(jQuery);
